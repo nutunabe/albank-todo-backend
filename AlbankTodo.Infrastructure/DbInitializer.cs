1c039e0a89ce1122935b8805579be018f09f66f7
nutunabe <baiytaan@gmail.com>
2022-11-07 16:18:20 +0900

    added db connection

---
+++ AlbankTodo.Infrastructure/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using AlbankTodo.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbankTodo.Infrastructure
+{
+    public class DbInitializer
+    {
+        public void Initialize(AlbankTodoContext context) =>
+            context.Database.EnsureCreated();
+    }
+}
---
+++ AlbankTodo.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using AlbankTodo.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbankTodo.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connString = configuration["DbConnection"];
+            services.AddDbContext<AlbankTodoContext>(options => options.UseNpgsql(connString));
+            services.AddScoped(provider => provider.GetService<AlbankTodoContext>());
+            return services;
+        }
+    }
+}
