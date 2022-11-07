7f4a843020f514e951c660d81e01fe2e835e32cd
nutunabe <baiytaan@gmail.com>
2022-11-07 13:28:32 +0900

    added todo task model

---
+++ AlbankTodo.Core/Entities/AlbankTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbankTodo.Core.Entities
+{
+    public enum Status { CREATED, IN_PROGRESS, COMPLETED }
+    public class AlbankTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime CompletedOn { get; set; }
+        public Status Status { get; set; }
+    }
+}
