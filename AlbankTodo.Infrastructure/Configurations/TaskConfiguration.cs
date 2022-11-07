3701e31176137f0048c5dca598afcc96700b36df
nutunabe <baiytaan@gmail.com>
2022-11-07 14:13:51 +0900

    added db context

---
+++ AlbankTodo.Infrastructure/Configurations/TaskConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlbankTodo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlbankTodo.Infrastructure.Configurations
+{
+    internal class TaskConfiguration : IEntityTypeConfiguration<AlbankTask>
+    {
+        public void Configure(EntityTypeBuilder<AlbankTask> builder)
+        {
+            builder.HasKey(task => task.Id);
+            builder.Property(task => task.Title).HasMaxLength(128);
+            builder.Property(task => task.Description).HasMaxLength(1024);
+            builder.Property(task => task.DueDate);
+            builder.Property(task => task.CreatedOn);
+            builder.Property(task => task.CompletedOn);
+            builder.Property(task => task.Status);
+        }
+    }
+}
---
+++ AlbankTodo.Infrastructure/Data/AlbankTodoContext.cs
@@ -0,0 +1,22 @@
+﻿using AlbankTodo.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbankTodo.Infrastructure.Data
+{
+    internal class AlbankTodoContext : DbContext
+    {
+        public AlbankTodoContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new TaskConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
